8cb9f858db0c08db7a8a0b3cba5455d924b6d7df
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2026-03-21 10:53:30 +0900

    Bump even to v0.4.0

--- Formula/even.rb
+++ Formula/even.rb
@@ -2,8 +2,8 @@ class Even < Formula
   desc "CLI to send text to Even Realities G2 smart glasses over Bluetooth"
   homepage "https://github.com/aluminumio/even"
   url "https://github.com/aluminumio/even/releases/latest/download/even-macos.tar.gz"
-  version "0.3.0"
-  sha256 "8258146d829899e65b99e4b2ae4020f5561808c2f42e6320969f693e80989bca"
+  version "0.4.0"
+  sha256 "33586d2172ac275b21aeeb0e27c59f7e528a36fd7c61d1315931e90ad1984dfb"
   license "MIT"
 
   depends_on :macos
